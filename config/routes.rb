7cbf23edb095ddce382f555856d532834199c344
Pablo Estrada <pablof300@gmail.com>
2018-12-01 15:38:39 -0500

    Added the authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root 'users#index'
   resources :users
   resources :trips
