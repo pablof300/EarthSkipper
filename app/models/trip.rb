112e80d493794403cb27828b4ad9dea555f6c2a2
Dylan <dylanbaker7@gmail.com>
2018-12-01 14:27:48 -0500

    Added has-many relatioship to trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,11 @@
 class Trip < ApplicationRecord
+
+  belongs_to :user
+
+  validates :verified, type: boolean, presence: true
+  validates :price, numericality: true, presence: true
+  validates :destination, presence: true
+  validates_date :startDate, presence: true
+  validates_date :returnDate, presence: true
+  
 end
