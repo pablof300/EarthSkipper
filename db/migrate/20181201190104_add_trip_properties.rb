9b72af222837057abd779d9d5314375d8c0c659d
Pablo Estrada <pablof300@gmail.com>
2018-12-01 18:13:16 -0500

    Fixed mess in migrations

--- db/migrate/20181201190104_add_trip_properties.rb
+++
@@ -1,9 +0,0 @@
-class AddTripProperties < ActiveRecord::Migration[5.2]
-  def change
-    add_column :trips,:verified
-    add_column :trips,:price
-    add_column :trips,:destination
-    add_column :trips,:startDate
-    add_column :trips,:returnDate
-  end
-end
--- db/migrate/20181201202116_add_user_id_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTrips < ActiveRecord::Migration[5.2]
-  def change
-    add_column :trips, :user_id, :integer
-  end
-end
