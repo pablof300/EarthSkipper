316d0bff10cc447e9acaee7547c2e12ad9ce75bd
Pablo Estrada <pablof300@gmail.com>
2018-12-02 05:17:33 -0500

    Added assets to heroku

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module EarthSkipper
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.serve_static_assets = true
   end
 end
