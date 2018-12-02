5ad1839a0319ed2aacf02373a6eb40a66e657f87
Pablo Estrada <pablof300@gmail.com>
2018-12-01 22:25:24 -0500

    Added show for both trip and user along with the i index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  root 'users#index'
+  root 'pages#index'
   resources :users
   resources :trips
 end
