39dd5a58973f57d32bbe6e58b96c0612dd034ed9
Pablo Estrada <pablof300@gmail.com>
2018-12-02 06:22:11 -0500

    Added dylan's code

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -255,7 +255,9 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
+    redirect_to login_path if !logged_in?
     @trips = Trip.all
+    @user = current_user
   end
 
   # GET /trips/1
